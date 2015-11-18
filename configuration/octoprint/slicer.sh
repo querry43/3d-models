05b7783b17d299d4a766a14d973213c91a5c8e40
Matt Harrington <querry43+github@underdogma.net>
2015-11-17 19:41:52 -0800

    pruning dead files, moving things into better places

--- configuration/octoprint/slicer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo ----
-echo curl http://slicer.underdogma.net/slice --form "stl_file=@$4" --create-dirs -o $6
-echo ----
-
-curl http://slicer.underdogma.net/slice --form "stl_file=@$4" --create-dirs -o $6
